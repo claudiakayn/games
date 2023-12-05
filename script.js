7c1e90e24d3921597ef39eba32b9ac70640897c7
clawkann <ckann@caltech.edu>
2023-12-04 16:52:37 -0800

    original page

---
+++ script.js
@@ -0,0 +1,8 @@
+function startGame() {
+    // Get the selected number of players
+    var selectedPlayers = document.getElementById("playerCount").value;
+
+    // You can use the selectedPlayers variable to initiate the game with the chosen player count
+    console.log("Starting game with " + selectedPlayers + " players");
+    // Add your game initiation logic here
+}
